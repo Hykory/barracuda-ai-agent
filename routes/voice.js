fa6764758d7146443c0e8cd7110c35b70246e2cd
Nate <keistak007@outlook.com>
2026-05-13 16:20:07 -0400

    Initial commit

---
+++ routes/voice.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/voice", (req, res) => {
+  const twiml = `
+<Response>
+  <Gather numDigits="1" action="/voice/language" method="POST" timeout="5">
+    <Say language="fr-CA" voice="Polly.Chantal" >Pour le français, appuyez sur 1.</Say>
+    <Say language="en-US" voice="Polly.Joanna" >For English, press 2.</Say>
+  </Gather>
+  <Redirect method="POST">/voice/language?Digits=1</Redirect>
+</Response>`;
+
+  res.type("text/xml");
+  res.send(twiml);
+});
+
+router.post("/voice/language", (req, res) => {
+  const digit = req.body.Digits || req.query.Digits || "1";
+  const lang = digit === "2" ? "en" : "fr";
+
+  const twiml = `
+<Response>
+  <Connect>
+    <Stream url="wss://stopping-absurd-nuzzle.ngrok-free.dev/ws">
+      <Parameter name="language" value="${lang}" />
+    </Stream>
+  </Connect>
+</Response>`;
+
+  res.type("text/xml");
+  res.send(twiml);
+});
+
+module.exports = router;
