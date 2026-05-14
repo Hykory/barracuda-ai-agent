738075061e66f313dcb6ee9dc3a5eee4d024c43a
Nate <keistak007@outlook.com>
2026-05-14 19:14:18 -0400

    voice4

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ wss.on("connection", (ws, req) => {
     aiSocket = new WebSocket(OPENAI_REALTIME_URL, {
       headers: {
         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        "OpenAI-Beta": "realtime=v1",
+       
       },
     });
 
